99eaf1f54947348e0e3ea0be5a53180f52520ecc
Guido Occhipinti <gocchipi@login01.g100.cineca.it>
2022-07-20 13:51:09 +0200

    update stochastic model script

--- seamless-notebooks-guido/setups/ogs/model_stoch.py
+++ seamless-notebooks-guido/setups/ogs/model_stoch.py
@@ -10,6 +10,7 @@ import subprocess
 #import statsmodels.api as sm
 from scipy.stats import variation
 import matplotlib.pyplot as plt
+import netCDF4 as nc
 try:
     from mpi4py import MPI
     comm  = MPI.COMM_WORLD
@@ -28,20 +29,23 @@ ensamble_counter=int(np.loadtxt('ensamble_counter.txt'))
 
 command='/g100_scratch/userexternal/gocchipi/BFM_5D/' + str(ensamble_counter).zfill(6) + '/'
 #output of the model for all samples
-ntrials = 1000
-#ntrials = 40   
-t_eval = np.linspace(0, 1825.*86400, 100000) 
+ntrials = 4000
+#ntrials = 4  
+D = 0.001  # Standard deviation.
+
+t_eval = np.linspace(0, 1825.*86400, 50000) 
 y = np.zeros((ntrials,len(t_eval),54))
 fig, ax = plt.subplots(3,3)
 ax = ax.ravel()
+fig1, ax1 = plt.subplots(3,3)
+ax1 = ax1.ravel()
 k=0
-l=[]
 
 subprocess.run(["mkdir","-p",command])
 subprocess.run(["cp","fabm.yaml",command])
 
 for s in range(ntrials):
-    r= s % nranks
+    r = s % nranks
     if r == rank :
       # Create model (loads fabm.yaml)
       model = pyfabm.Model('fabm.yaml')
@@ -75,8 +79,6 @@ for s in range(ntrials):
       
       #variables with noise
       varnames = ["B1/c","P1/c","P2/c","P3/c","P4/c","Z5/c","Z6/c","Z3/c","Z4/c"]
-      #noise
-      D = 0.00001  # Standard deviation.
       
       # Time-integrate over 1000 days (note: FABM's internal time unit is seconds!)
       dt = (t_eval[-1]-t_eval[0])/len(t_eval)
@@ -93,52 +95,77 @@ for s in range(ntrials):
                   else:
                       y[s,i,j]=y[s,i-1,j]+dy[j]*dt
           model.state[:]=y[s,i,:]
-      #if s!=ntrials-1:
-      #    del model
 if rank == 0:
     y_global = np.copy(y)
-    val = np.zeros(y.shape)
     for s in range(ntrials):
         r = s % nranks
         if r !=0 :
-          print(r)
-          comm.Recv( [val[:], MPI.DOUBLE], source=r, tag=1 )
-          y_global[s,:] = val[s,:]
+          rea = comm.recv(source=r, tag=11)
+          y_global[int(rea['idx']),:,:] = rea['data']
+        comm.Barrier()
 else:
-    val = np.zeros(y.shape)
     for s in range(ntrials):
         r = s % nranks
+        rea = {'idx':s, 'data': y[s,:,:]}
         if r == rank :
-            comm.Send( [y[s,:], MPI.DOUBLE], dest=0, tag=1 )
+            comm.send(rea, dest=0, tag=11)
+        comm.Barrier()
 comm.Barrier()
 print('End of the loop',flush=True)
 
 if rank == 0 :
+  t_eval = t_eval / 86400
   y = np.copy(y_global)  
   # We create bins for the histograms.
-  bins = np.linspace(-2., 50., 200)
+  bins = np.linspace(0., 50., 100)
   
   # We display the histogram for a few points in
    # time
   for i in range(len(t_eval)):
         for j in range(len(model.state)):
-            if i in (50, 5000, 9000):# and s == ntrials-1:
+            if i in (0, 10000, 49000):# and s == ntrials-1:
               if model.state_variables[j].name in varnames:
                 hist1, _ = np.histogram(y[:,i,j], bins=bins)
-                ax[k].plot((bins[1:] + bins[:-1]) / 2, hist1/ntrials,
-                    {50: '-', 5000: '.', 9000: '-.', }[i],
-                    label=f"t={i * dt / 86400:.2f}")
-                l.append(f"t={i * dt / 86400:.2f}")
-                ax[k].set_title(model.state_variables[j].name,fontsize=6)
+                ax[k].plot((_[1:] + _[:-1]) / 2, hist1/ntrials,
+                #ax[k].plot((bins[1:] + bins[:-1]) / 2, hist1/ntrials,
+                    {0: '-', 10000: '.', 49000: '-.', }[i],
+                    label=f"t={i * dt / 86400:.2f}"+" d")
+                ax[k].set_title(model.state_variables[j].name,fontsize=8)
                 ax[8].legend()
+                ax[k].tick_params(axis='both', which='major', labelsize=7)
+                ax[k].tick_params(axis='both', which='minor', labelsize=7)
                 ax[k].label_outer()
+                #my_xticks = ax[k].get_xticks()
+                #ax[k].set_xticks([my_xticks[0], my_xticks[-1]])#, visible=True, rotation="horizontal")
                 k+=1
         k=0
-  #labels = l[:3]
-  #fig.legend(l, labels,ncol=2, loc='upper center')
+  #get deterministic trajectory
+  ncname = 'result_ode.nc'
+  f = nc.Dataset(ncname)
+  varnames_ = []
+  for v in varnames:
+   varnames_.append(v.replace("/","_"))
+  z = np.zeros((len(varnames),len(t_eval)))
+  for it,tname in enumerate(varnames_):  
+            z[it] = f.variables[tname][:,0,0,0]
+  for j in range(len(model.state)):
+    if model.state_variables[j].name in varnames:
+        ax1[k].plot(t_eval,np.mean(y[:,:,j],0),c='black',label='stoch')
+        #ax1[k].plot(t_eval,np.amax(y[:,:,j],0),c='red')
+        #ax1[k].plot(t_eval,np.amin(y[:,:,j],0),c='red')
+        ax1[k].tick_params(axis='both', which='major', labelsize=7)
+        ax1[k].tick_params(axis='both', which='minor', labelsize=7)
+        ax1[k].plot(t_eval,z[k,:],c='blue',label='det')
+        ax1[k].set_title(model.state_variables[j].name,fontsize=8)
+        ax1[8].legend()
+        k+=1
+  fig.text(0.5, 0.01, 'Concentration [$mg C/m^3$]', ha='center')
+  fig.text(0.01, 0.5, 'PDF', va='center', rotation='vertical')
   fig.savefig('stoch_histo.png', format='png',dpi=150)
-  
-  
+  fig1.text(0.5, 0.01, 'time [days]', ha='center')
+  #fig1.text(0.01, 0.5, 'Mean Concentration [$mg C/m^3$]', va='center', rotation='vertical')
+  fig1.tight_layout()
+  fig1.savefig('stoch_mean.png', format='png',dpi=150)
   #Relaxation time
   tau = np.zeros(len(varnames))
   for i in range(len(t_eval)):
@@ -147,8 +174,12 @@ if rank == 0 :
           if model.state_variables[j].name in varnames:
               tau[k]+=np.abs(np.mean(y[:,i,j],axis=0)-np.mean(y[:,-1,j],axis=0)) / np.abs(np.mean(y[:,0,j],axis=0)-np.mean(y[:,-1,j],axis=0))    
               k+=1
-  
+#  for j in range(len(model.state)):
+#     if model.state_variables[j].name in varnames:
+#        print(np.var(y[:,-1,j],axis=0))
+  print(D)
   print(tau)
+  print(y[:,0,50])
   ##t_eval = np.linspace(0, 3650.*86400, 300000) 
 ##sol = scipy.integrate.solve_ivp(dy, [0., 3650.*86400], model.state, t_eval=t_eval)
 ##y = scipy.integrate.odeint(dy, model.state, t*86400)
@@ -166,43 +197,50 @@ if rank == 0 :
 #laststeps = int(Nt/10) #compute the indicators just for this last timesteps
 #freq = (1/deltaT) * np.linspace(0,laststeps/2,int(laststeps/2)) / laststeps
 #
-## Save results
-#
-#
-#fileoutput = 'result.nc'
-#f = nc.Dataset(fileoutput, mode='w')
-#
-#lat_dim = f.createDimension('lat', 1)
-#lon_dim = f.createDimension('lon', 1)
-#dep_dim = f.createDimension('z', 1)
-#time_dim = f.createDimension('time', Nt)
-#
-#lat = f.createVariable('lat', np.float32, ('lat',))
-#lat.units = 'degrees_north'
-#lat.long_name = 'latitude'
-#f.variables['lat'][:]=0
-#
-#lon = f.createVariable('lon', np.float32, ('lon',))
-#lon.units = 'degrees_east'
-#lon.long_name = 'longitude'
-#f.variables['lon'][:]=0
-#
-#time = f.createVariable('time', np.float64, ('time',))
-#time.units = 'days'
-#time.long_name = 'time'
-#f.variables['time'][:]=t
-#
-#depth = f.createVariable('z', np.float32, ('z',))
-#depth.units = 'meters'
-#depth.long_name = 'depth'
-#f.variables['z'][:]=1
-#
-#for v,variable in enumerate(model.state_variables):
-#   ncvar = variable.name.replace("/","_")
-#   var = f.createVariable(ncvar, np.float64, ('time', 'z','lat','lon'))
-#   var.units = variable.units
-#   var.long_name = variable.long_name
-#   f.variables[ncvar][:]=y[:,v]
+# Save results
+
+
+fileoutput = 'result.nc'
+f = nc.Dataset(fileoutput, mode='w')
+
+lat_dim = f.createDimension('lat', 1)
+lon_dim = f.createDimension('lon', 1)
+dep_dim = f.createDimension('z', 1)
+time_dim = f.createDimension('time', t_eval.shape[0])
+#n=np.arange(0,ntrials,1)
+#sample_dim = f.createDimension('n', n.shape[0])
+
+lat = f.createVariable('lat', np.float32, ('lat',))
+lat.units = 'degrees_north'
+lat.long_name = 'latitude'
+f.variables['lat'][:]=0
+
+lon = f.createVariable('lon', np.float32, ('lon',))
+lon.units = 'degrees_east'
+lon.long_name = 'longitude'
+f.variables['lon'][:]=0
+
+time = f.createVariable('time', np.float64, ('time',))
+time.units = 'days'
+time.long_name = 'time'
+f.variables['time'][:]=t_eval
+
+depth = f.createVariable('z', np.float32, ('z',))
+depth.units = 'meters'
+depth.long_name = 'depth'
+f.variables['z'][:]=1
+
+#sample = f.createVariable('n', np.float64, ('z',))
+#sample.units = ''
+#sample.long_name = 'sample'
+#f.variables['n'][:]=n
+
+for v,variable in enumerate(model.state_variables):
+   ncvar = variable.name.replace("/","_")
+   var = f.createVariable(ncvar, np.float64, ('time', 'z','lat','lon'))
+   var.units = variable.units
+   var.long_name = variable.long_name
+   f.variables[ncvar][:]=np.mean(y[:,:,v], axis=0)
 #
 ## Sensibility targets
 #
@@ -289,5 +327,5 @@ if rank == 0 :
 ##    P1_fstate.units = ''
 ##    P1_fstate.long_name = variable.name.replace("/","_") + "_final_state"
 ##    f.variables[ncfstate][:]=fstate
-#f.close()
+f.close()
 #subprocess.run(["cp","result.nc",command])
