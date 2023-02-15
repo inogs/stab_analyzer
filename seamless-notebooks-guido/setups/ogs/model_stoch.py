b4f1da432ec83597ef70baf9c80ccf83b533f801
Guido Occhipinti <gocchipi@login01.g100.cineca.it>
2023-02-15 15:37:15 +0100

    added T and L cycles

--- seamless-notebooks-guido/setups/ogs/model_stoch.py
+++ seamless-notebooks-guido/setups/ogs/model_stoch.py
@@ -11,6 +11,20 @@ import subprocess
 from scipy.stats import variation
 import matplotlib.pyplot as plt
 import netCDF4 as nc
+from scipy import signal
+from scipy.stats import pearsonr
+
+import copy
+def signalPower(x):
+    return np.mean(x**2)
+def SNRsystem(inputSig, outputSig):
+    noise = outputSig-inputSig
+
+    powS = signalPower(outputSig)
+    powN = signalPower(noise)
+    return 10*np.log10((powS-powN)/powN)
+
+
 try:
     from mpi4py import MPI
     comm  = MPI.COMM_WORLD
@@ -27,13 +41,13 @@ print(isParallel, flush=True)
 
 ensamble_counter=int(np.loadtxt('ensamble_counter.txt'))
 
-command='/g100_scratch/userexternal/gocchipi/BFM_5D/' + str(ensamble_counter).zfill(6) + '/'
+command='/g100_scratch/userexternal/gocchipi/BFM_STO/' + str(ensamble_counter).zfill(6) + '/'
 #output of the model for all samples
-ntrials = 4000
-#ntrials = 4  
-D = 0.001  # Standard deviation.
+#ntrials = 100
+ntrials = 10  
+#D = 0.001  # Standard deviation.
 
-t_eval = np.linspace(0, 1825.*86400, 50000) 
+t_eval = np.linspace(0, 3650.*86400, 50000) 
 y = np.zeros((ntrials,len(t_eval),54))
 fig, ax = plt.subplots(3,3)
 ax = ax.ravel()
@@ -41,8 +55,50 @@ fig1, ax1 = plt.subplots(3,3)
 ax1 = ax1.ravel()
 k=0
 
-subprocess.run(["mkdir","-p",command])
-subprocess.run(["cp","fabm.yaml",command])
+##########sinusoidal temperature fluctuation
+# parameters
+MeanTemp = 15           # Average temperature in the country
+DailyAmpl = 5           # Amplitude of the daily cycle
+YearlyAmpl = 5           # Amplitude of the yearly cycle
+#oiseStd = 0.1          # Standard deviation of normally distributed error term
+
+# Total seconds in year
+TotalHours = 24*365*60*60 #year period
+tau        = 24*60*60     #day period
+
+# Generate the frequency components of the data
+DailyCycle = -DailyAmpl*np.cos( (2*np.pi)*t_eval/tau )
+YearlyCycle = -YearlyAmpl*np.cos( (2*np.pi)*t_eval/TotalHours )
+#Noise = np.random.normal(0, NoiseStd, TotalHours)
+
+# Final series
+T = MeanTemp + DailyCycle + YearlyCycle #+ Noise
+
+
+
+
+##########sinusoidal light fluctuation
+# parameters
+MeanTemp =  0           # Average temperature in the country
+DailyAmpl = 100          # Amplitude of the daily cycle
+YearlyAmpl = 10         # Amplitude of the yearly cycle
+#NoiseStd = 0.1          # Standard deviation of normally distributed error term
+
+# Total seconds in year
+TotalHours = 24*365*60*60 #year period
+tau        = 24*60*60     #day period
+
+# Generate the frequency components of the data
+DailyCycle = -DailyAmpl*np.cos( (2*np.pi)*t_eval/tau )
+YearlyCycle = -YearlyAmpl*np.cos( (2*np.pi)*t_eval/TotalHours )
+#Noise = np.random.normal(0, NoiseStd, TotalHours)
+
+# Final series
+L = np.where(MeanTemp + DailyCycle + YearlyCycle > 0, MeanTemp + DailyCycle + YearlyCycle, 0.)#+ Noise
+
+
+#subprocess.run(["mkdir","-p",command])
+#subprocess.run(["cp","fabm.yaml",command])
 
 for s in range(ntrials):
     r = s % nranks
@@ -53,7 +109,7 @@ for s in range(ntrials):
       # Configure the environment
       # Note: the set of environmental dependencies depends on the loaded biogeochemical model.
       model.dependencies['cell_thickness'].value = 1.
-      model.dependencies['temperature'].value = 15.
+      model.dependencies['temperature'].value = T[0]#15.
       model.dependencies['practical_salinity'].value = 30.
       model.dependencies['density'].value = 1000.
       model.dependencies['depth'].value = 1.
@@ -61,7 +117,7 @@ for s in range(ntrials):
       model.dependencies['isBen'].value = 1.
       model.dependencies['longitude'].value = 0.
       model.dependencies['latitude'].value = 0.
-      model.dependencies['surface_downwelling_shortwave_flux'].value = 50.
+      model.dependencies['surface_downwelling_shortwave_flux'].value = L[0]#50.
       model.dependencies['surface_air_pressure'].value = 1.
       model.dependencies['wind_speed'].value = 5.
       model.dependencies['mole_fraction_of_carbon_dioxide_in_air'].value = 390.
@@ -78,7 +134,15 @@ for s in range(ntrials):
       #    return model.getRates()
       
       #variables with noise
-      varnames = ["B1/c","P1/c","P2/c","P3/c","P4/c","Z5/c","Z6/c","Z3/c","Z4/c"]
+#     varnames = ["Z3/c"]
+      varnames = ["B1/c","P1/c","P2/c","P3/c","P4/c","Z3/c","Z4/c","Z5/c","Z6/c"]
+
+      #noise std deviation
+
+      for i,v in enumerate(model.parameters):
+         if 'D_noise' in v.name:
+              D = v.getValue()
+      print('noise std ', D)
       
       # Time-integrate over 1000 days (note: FABM's internal time unit is seconds!)
       dt = (t_eval[-1]-t_eval[0])/len(t_eval)
@@ -87,11 +151,28 @@ for s in range(ntrials):
       
       #print(model.state) 
       for i in range(len(t_eval)):
+          model.dependencies['cell_thickness'].value = 1.
+          model.dependencies['temperature'].value = T[i]
+          model.dependencies['practical_salinity'].value = 30.
+          model.dependencies['density'].value = 1000.
+          model.dependencies['depth'].value = 1.
+          model.dependencies['pressure'].value = 1.
+          model.dependencies['isBen'].value = 1.
+          model.dependencies['longitude'].value = 0.
+          model.dependencies['latitude'].value = 0.
+          model.dependencies['surface_downwelling_shortwave_flux'].value = L[i]
+          model.dependencies['surface_air_pressure'].value = 1.
+          model.dependencies['wind_speed'].value = 5.
+          model.dependencies['mole_fraction_of_carbon_dioxide_in_air'].value = 390.
+          model.dependencies['number_of_days_since_start_of_the_year'].value = 1.
           dy = model.getRates()
           for j in range(len(model.state)):
               if i!=0:
                   if model.state_variables[j].name in varnames:
-                      y[s,i,j]=y[s,i-1,j]+dy[j]*dt + D * sqrtdt * np.random.randn() * y[s,i-1,j]
+                      if i<15000:    #stop noise
+                          y[s,i,j]=y[s,i-1,j]+dy[j]*dt  + D * sqrtdt * np.random.randn() * y[s,i-1,j]
+                      else:
+                          y[s,i,j]=y[s,i-1,j]+dy[j]*dt
                   else:
                       y[s,i,j]=y[s,i-1,j]+dy[j]*dt
           model.state[:]=y[s,i,:]
@@ -119,213 +200,125 @@ if rank == 0 :
   # We create bins for the histograms.
   bins = np.linspace(0., 50., 100)
   
-  # We display the histogram for a few points in
-   # time
-  for i in range(len(t_eval)):
-        for j in range(len(model.state)):
-            if i in (0, 10000, 49000):# and s == ntrials-1:
-              if model.state_variables[j].name in varnames:
-                hist1, _ = np.histogram(y[:,i,j], bins=bins)
-                ax[k].plot((_[1:] + _[:-1]) / 2, hist1/ntrials,
-                #ax[k].plot((bins[1:] + bins[:-1]) / 2, hist1/ntrials,
-                    {0: '-', 10000: '.', 49000: '-.', }[i],
-                    label=f"t={i * dt / 86400:.2f}"+" d")
-                ax[k].set_title(model.state_variables[j].name,fontsize=8)
-                ax[8].legend()
-                ax[k].tick_params(axis='both', which='major', labelsize=7)
-                ax[k].tick_params(axis='both', which='minor', labelsize=7)
-                ax[k].label_outer()
-                #my_xticks = ax[k].get_xticks()
-                #ax[k].set_xticks([my_xticks[0], my_xticks[-1]])#, visible=True, rotation="horizontal")
-                k+=1
-        k=0
   #get deterministic trajectory
   ncname = 'result_ode.nc'
   f = nc.Dataset(ncname)
+  varnames = ["B1/c","P1/c","P2/c","P3/c","P4/c","Z3/c","Z4/c","Z5/c","Z6/c"]
   varnames_ = []
   for v in varnames:
    varnames_.append(v.replace("/","_"))
   z = np.zeros((len(varnames),len(t_eval)))
   for it,tname in enumerate(varnames_):  
             z[it] = f.variables[tname][:,0,0,0]
-  for j in range(len(model.state)):
-    if model.state_variables[j].name in varnames:
-        ax1[k].plot(t_eval,np.mean(y[:,:,j],0),c='black',label='stoch')
-        #ax1[k].plot(t_eval,np.amax(y[:,:,j],0),c='red')
-        #ax1[k].plot(t_eval,np.amin(y[:,:,j],0),c='red')
-        ax1[k].tick_params(axis='both', which='major', labelsize=7)
-        ax1[k].tick_params(axis='both', which='minor', labelsize=7)
-        ax1[k].plot(t_eval,z[k,:],c='blue',label='det')
-        ax1[k].set_title(model.state_variables[j].name,fontsize=8)
-        ax1[8].legend()
-        k+=1
-  fig.text(0.5, 0.01, 'Concentration [$mg C/m^3$]', ha='center')
-  fig.text(0.01, 0.5, 'PDF', va='center', rotation='vertical')
-  fig.savefig('stoch_histo.png', format='png',dpi=150)
-  fig1.text(0.5, 0.01, 'time [days]', ha='center')
-  #fig1.text(0.01, 0.5, 'Mean Concentration [$mg C/m^3$]', va='center', rotation='vertical')
-  fig1.tight_layout()
-  fig1.savefig('stoch_mean.png', format='png',dpi=150)
-  #Relaxation time
-  tau = np.zeros(len(varnames))
-  for i in range(len(t_eval)):
-      k=0
-      for j in range(len(model.state)):
-          if model.state_variables[j].name in varnames:
-              tau[k]+=np.abs(np.mean(y[:,i,j],axis=0)-np.mean(y[:,-1,j],axis=0)) / np.abs(np.mean(y[:,0,j],axis=0)-np.mean(y[:,-1,j],axis=0))    
-              k+=1
-#  for j in range(len(model.state)):
-#     if model.state_variables[j].name in varnames:
-#        print(np.var(y[:,-1,j],axis=0))
-  print(D)
-  print(tau)
-  print(y[:,0,50])
-  ##t_eval = np.linspace(0, 3650.*86400, 300000) 
-##sol = scipy.integrate.solve_ivp(dy, [0., 3650.*86400], model.state, t_eval=t_eval)
-##y = scipy.integrate.odeint(dy, model.state, t*86400)
-#t = t_eval/86400
-#
-## Plot results
-##import pylab
-##pylab.plot(t, y)
-##pylab.legend([variable.path for variable in model.state_variables])
-##pylab.show()
-#
-#
-#Nt=t.shape[0]
-#deltaT=t[1]-t[0]
-#laststeps = int(Nt/10) #compute the indicators just for this last timesteps
-#freq = (1/deltaT) * np.linspace(0,laststeps/2,int(laststeps/2)) / laststeps
-#
-# Save results
 
 
-fileoutput = 'result.nc'
-f = nc.Dataset(fileoutput, mode='w')
-
-lat_dim = f.createDimension('lat', 1)
-lon_dim = f.createDimension('lon', 1)
-dep_dim = f.createDimension('z', 1)
-time_dim = f.createDimension('time', t_eval.shape[0])
-#n=np.arange(0,ntrials,1)
-#sample_dim = f.createDimension('n', n.shape[0])
-
-lat = f.createVariable('lat', np.float32, ('lat',))
-lat.units = 'degrees_north'
-lat.long_name = 'latitude'
-f.variables['lat'][:]=0
-
-lon = f.createVariable('lon', np.float32, ('lon',))
-lon.units = 'degrees_east'
-lon.long_name = 'longitude'
-f.variables['lon'][:]=0
-
-time = f.createVariable('time', np.float64, ('time',))
-time.units = 'days'
-time.long_name = 'time'
-f.variables['time'][:]=t_eval
-
-depth = f.createVariable('z', np.float32, ('z',))
-depth.units = 'meters'
-depth.long_name = 'depth'
-f.variables['z'][:]=1
-
-#sample = f.createVariable('n', np.float64, ('z',))
-#sample.units = ''
-#sample.long_name = 'sample'
-#f.variables['n'][:]=n
-
-for v,variable in enumerate(model.state_variables):
-   ncvar = variable.name.replace("/","_")
-   var = f.createVariable(ncvar, np.float64, ('time', 'z','lat','lon'))
-   var.units = variable.units
-   var.long_name = variable.long_name
-   f.variables[ncvar][:]=np.mean(y[:,:,v], axis=0)
-#
-## Sensibility targets
-#
-##critical lenght for cycles
-#epsilon = 0.001
-#for v,variable in enumerate(model.state_variables):
-#
-##coeff of variance
-#    if math.isinf(variation(y[-laststeps:,v])) : 
-#        p1_var=int(1)
-#    elif variation(y[-laststeps:,v]) > epsilon :
-#        p1_var = int(1)
-#    else :
-#        p1_var = int(0)
-#
-##Relative peaks in the fourier spac
-#    PF = fft(y[-laststeps:,v])
-#
-#    PF2 = np.abs(PF / laststeps) #full spectrum  /Npoints to get the true amplitudes
-#    PF1 = PF2[:laststeps//2]              #half spectrum
-#    PF1[1:] = 2*PF1[1:]           #actual amplitude
-#
-#    #find peaks
-#    peak_index0, dict_vals = find_peaks(np.concatenate(([min(PF1)],PF1,[min(PF1)])), distance=5)
-#    #find the biggest two
-#    peak_index0 = peak_index0-1
-#    heights0 = np.sort(PF1[peak_index0])[::-1]
-#    if len(heights0) >1 :
-#        if heights0[0] >0.001 :
-#            p1_peak = heights0[1]/heights0[0]
-#        else :
-#            p1_peak = 0
-#    else :
-#        p1_peak = 0
-#    #make it discrete
-#    if p1_peak > epsilon :
-#        p1_peak = int(1)
-#    else :
-#        p1_peak = int(0)
-#
-##fourier spectrum plot
-##    if v==1:
-##        pylab.plot(freq,PF1)
-##        pylab.show()
-#
-##Relaxation time
-##    final_times = int( Nt/10)
-##    yfinP = np.mean(y[-final_times:,v])
-##    for i,Y in enumerate(y[-laststeps:,v]):
-##        p1_tau =+ np.abs( Y - yfinP ) / np.abs(y[0,v]-yfinP)
-#
-##Autocorrelation
-#
-##    p1_acf = sm.tsa.acf(y[-laststeps:,v], nlags=1)[1]
-##Last state
-##    fstate = y[-1,v]
-#
-#    ncvar = variable.name.replace("/","_") + "_var"
-#    P1_var = f.createVariable(ncvar, np.float32, ('z','lat','lon'))
-#    P1_var.units = ' '
-#    P1_var.long_name = ncvar + 'iance'
-#    f.variables[ncvar][:]=p1_var
-#
-#    ncpeak = variable.name.replace("/","_") + "_peak"
-#    P1_peak = f.createVariable(ncpeak, np.float32, ('z','lat','lon'))
-#    P1_peak.units = ' '
-#    P1_peak.long_name = ncpeak +'_height'
-#    f.variables[ncpeak][:]=p1_peak
-#
-##    nctau = variable.name.replace("/","_") + "_tau"
-##    P1_tau = f.createVariable(nctau, np.float32, ('z','lat','lon'))
-##    P1_tau.units = 'days'
-##    P1_tau.long_name = variable.name.replace("/","_") + "_relaxation_time"
-##    f.variables[nctau][:]=p1_tau
-#
-##    nctau = variable.name.replace("/","_") + "_acf"
-##    P1_acf = f.createVariable(nctau, np.float32, ('z','lat','lon'))
-##    P1_acf.units = ''
-##    P1_acf.long_name = variable.name.replace("/","_") + "_lag1_autocorrelation"
-##    f.variables[nctau][:]=p1_acf
-#    
-##    ncfstate = variable.name.replace("/","_") + "_fstate"
-##    P1_fstate = f.createVariable(ncfstate, np.float32, ('z','lat','lon'))
-##    P1_fstate.units = ''
-##    P1_fstate.long_name = variable.name.replace("/","_") + "_final_state"
-##    f.variables[ncfstate][:]=fstate
-f.close()
+  
+  # Save results
+  
+  
+# fileoutput = 'result.nc'
+  fileoutput = 'result_4.nc'
+  f = nc.Dataset(fileoutput, mode='w')
+  
+  lat_dim = f.createDimension('lat', 1)
+  lon_dim = f.createDimension('lon', 1)
+  dep_dim = f.createDimension('z', 1)
+  time_dim = f.createDimension('time', t_eval.shape[0])
+  #n=np.arange(0,ntrials,1)
+  #sample_dim = f.createDimension('n', n.shape[0])
+  
+  lat = f.createVariable('lat', np.float32, ('lat',))
+  lat.units = 'degrees_north'
+  lat.long_name = 'latitude'
+  f.variables['lat'][:]=0
+  
+  lon = f.createVariable('lon', np.float32, ('lon',))
+  lon.units = 'degrees_east'
+  lon.long_name = 'longitude'
+  f.variables['lon'][:]=0
+  
+  time = f.createVariable('time', np.float64, ('time',))
+  time.units = 'days'
+  time.long_name = 'time'
+  f.variables['time'][:]=t_eval
+  
+  depth = f.createVariable('z', np.float32, ('z',))
+  depth.units = 'meters'
+  depth.long_name = 'depth'
+  f.variables['z'][:]=1
+  
+  temp = f.createVariable('temp', np.float64, ('time',))
+  temp.units = 'C'
+  temp.long_name = 'temperature in celsius'
+  f.variables['temp'][:]=T
+  #sample = f.createVariable('n', np.float64, ('z',))
+  #sample.units = ''
+  #sample.long_name = 'sample'
+  #f.variables['n'][:]=n
+  
+  for v,variable in enumerate(model.state_variables):
+     ncvar = variable.name.replace("/","_")
+     var = f.createVariable(ncvar, np.float64, ('time', 'z','lat','lon'))
+     var.units = variable.units
+     var.long_name = variable.long_name
+     f.variables[ncvar][:]=np.mean(y[:,:,v], axis=0)
+  #
+  # sensitivity targets
+  laststeps = int(len(t_eval)/2.)
+  # cv
+  y = np.mean(y,axis=0)
+  kkk=0
+  for v,variable in enumerate(model.state_variables):
+     ncvar = variable.name.replace("/","_")
+     if ncvar in varnames_:
+         cv = (variation(y[-laststeps:,v])-variation(z[kkk,-laststeps:]))/D
+  #      cv = pearsonr(y[-laststeps:,v], z[kkk,-laststeps:])[0]
+  #      mean = np.mean(y[-laststeps:,v])/np.mean(z[kkk,-laststeps:])
+         mean = np.mean(z[kkk,-laststeps:-100]*y[-(laststeps-100):,v])
+         SNR = SNRsystem(y[-laststeps:,v], z[kkk,-laststeps:])
+         if y[:,v].min() < 0.0001:
+             ext=1
+         else:
+             ext=0
+  #fourier sectrum
+  #  if ncvar == 'Z6_c':
+  #      plt.clf()
+  #      PF = fft(y[-laststeps:,v])
+  #      deltaT=t_eval[1]-t_eval[0]
+  #      freq = (1/deltaT) * np.linspace(0,laststeps/2,int(laststeps/2)) / laststeps
+  #      PF2 = np.abs(PF / laststeps) #full spectrum  /Npoints to get the true amplitudes
+  #      PF1 = PF2[:laststeps//2]              #half spectrum
+  #      PF1[1:] = 2*PF1[1:]
+  #      plt.plot(freq,PF1)
+  #      plt.show()
+         
+         ncfstate = variable.name.replace("/","_") + "_cv" 
+         P1_fstate = f.createVariable(ncfstate, np.float32, ('z','lat','lon'))
+         P1_fstate.units = ''
+         P1_fstate.long_name = variable.name.replace("/","_") + "_cv"
+         f.variables[ncfstate][:]=cv
+         print(ncfstate,cv)
+  
+         ncfstate = variable.name.replace("/","_") + "_mean"
+         P1_fstate = f.createVariable(ncfstate, np.float32, ('z','lat','lon'))
+         P1_fstate.units = ''
+         P1_fstate.long_name = variable.name.replace("/","_") + "_mean"
+         f.variables[ncfstate][:]=mean
+         print(ncfstate,mean)
+  
+         ncfstate = variable.name.replace("/","_") + "_snr"
+         P1_fstate = f.createVariable(ncfstate, np.float32, ('z','lat','lon'))
+         P1_fstate.units = ''
+         P1_fstate.long_name = variable.name.replace("/","_") + "_snr"
+         f.variables[ncfstate][:]=SNR
+         print(ncfstate,SNR)
+         print('*****')
+  
+         ncfstate = variable.name.replace("/","_") + "_ext"
+         P1_fstate = f.createVariable(ncfstate, np.float32, ('z','lat','lon'))
+         P1_fstate.units = ''
+         P1_fstate.long_name = variable.name.replace("/","_") + "_ext"
+         f.variables[ncfstate][:]=ext
+  
+  f.close()
 #subprocess.run(["cp","result.nc",command])
