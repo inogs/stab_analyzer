2403e90aa8e1549c707264fa9041b12f99fb7804
Guido Occhipinti <gocchipi@login10.g100.cineca.it>
2024-01-15 18:13:07 +0100

    script update

--- ExternalForcings/Scripts/methods.py
+++ ExternalForcings/Scripts/methods.py
@@ -7,8 +7,14 @@ import pandas as pd
 files = ['result_chaos.nc','result_periodic.nc','noise_result_stationary.nc']
 conf = ['endogenous chaos',r'induced chaos $5^\circ C$','stoch temp']
 colors = ['k','b','r']
-fig,axs = plt.subplots(1,2)
+
+#HC for logistic map with parameter a=4 --> chaotic
+H_logistic = 0.7387519146337446
+C_logistic = 0.2918153116452781
+
+fig,axs = plt.subplots(2,2,figsize=(5,6))
 fig1,ax1 = plt.subplots(3,1)
+axs = axs.ravel()
 ax1 = ax1.ravel()
 for ifc,ncname in enumerate(files):
         
@@ -46,36 +52,51 @@ for ifc,ncname in enumerate(files):
     C = np.zeros(len(taux))
     for it,tx in enumerate(taux):
         H[it],C[it] = od.complexity_entropy(P1,taux=tx)
-    axs[0].plot(H,C,c=colors[ifc],label=conf[ifc])
-    axs[1].plot(taux/5000*365,H,c=colors[ifc],linestyle='dashed',label=r'$H_{S}$')
-    axs[1].plot(taux/5000*365,C,c=colors[ifc],linestyle='dotted',label=r'$C_{JS}$')
-    if 'periodic' in ncname:
-        P1 = f_noise.variables['P1_c'][19,-80000:,0,0,0]
-        P1_1 = P1[:20000]
-        P1_2 = P1[20000:40000]
-        P1_3 = P1[40000:60000]
-        P1_4 = P1[60000:]
-        P1_1 = P1_2 - P1_1
-        P1_2 = P1_3 - P1_4
-        P1 = (P1_1+P1_2)/2
-        taux = np.linspace(1,5000,100,dtype=int)
-        H = np.zeros(len(taux))
-        C = np.zeros(len(taux))
-        for it,tx in enumerate(taux):
-            H[it],C[it] = od.complexity_entropy(P1,taux=tx)
-        axs[0].plot(H,C,c='cyan',label=r'induced chaos $19^\circ C$')
-        axs[1].plot(taux/5000*365,H,c='cyan',linestyle='dashed',label=r'$H_{S}$')
-        axs[1].plot(taux/5000*365,C,c='cyan',linestyle='dotted',label=r'$C_{JS}$')
+    axs[ifc].plot(H,C,c=colors[ifc],label=conf[ifc])
+    axs[ifc].scatter(H_logistic,C_logistic,c='g')
+    axs[3].plot(taux/5000*365,H,c=colors[ifc],linestyle='dashed',label=r'$H_{S}$')
+    axs[3].plot(taux/5000*365,C,c=colors[ifc],linestyle='dotted',label=r'$C_{JS}$')
+#   if 'periodic' in ncname:
+#       P1 = f_noise.variables['P1_c'][19,-80000:,0,0,0]
+#       P1_1 = P1[:20000]
+#       P1_2 = P1[20000:40000]
+#       P1_3 = P1[40000:60000]
+#       P1_4 = P1[60000:]
+#       P1_1 = P1_2 - P1_1
+#       P1_2 = P1_3 - P1_4
+#       P1 = (P1_1+P1_2)/2
+#       taux = np.linspace(1,5000,100,dtype=int)
+#       H = np.zeros(len(taux))
+#       C = np.zeros(len(taux))
+#       for it,tx in enumerate(taux):
+#           H[it],C[it] = od.complexity_entropy(P1,taux=tx)
+#       axs[0].plot(H,C,c='cyan',label=r'induced chaos $19^\circ C$')
+#       axs[1].plot(taux/5000*365,H,c='cyan',linestyle='dashed',label=r'$H_{S}$')
+#       axs[1].plot(taux/5000*365,C,c='cyan',linestyle='dotted',label=r'$C_{JS}$')
 
 
 axs[0].set_ylabel(r'$C_{JS}$')
+axs[1].set_ylabel(r'$C_{JS}$')
+axs[2].set_ylabel(r'$C_{JS}$')
 axs[0].set_xlabel(r'$H_{S}$')
-axs[0].legend()
+axs[1].set_xlabel(r'$H_{S}$')
+axs[2].set_xlabel(r'$H_{S}$')
+axs[0].legend(loc='upper center', bbox_to_anchor=(0.5, 1.35),
+          ncol=1)
+axs[1].legend(loc='upper center', bbox_to_anchor=(0.5, 1.35),
+          ncol=1)
+axs[2].legend(loc='upper center', bbox_to_anchor=(0.5, 1.35),
+          ncol=1)
 axs[0].set_xlim(0.0,1.0)
+axs[1].set_xlim(0.0,1.0)
+axs[2].set_xlim(0.0,1.0)
 axs[0].set_ylim(0.0,0.5)
-axs[1].set_ylabel(r'$H_{S},C_{JS}$')
-axs[1].set_xlabel(r'embedding delay $[days]$')
-axs[1].legend()
+axs[1].set_ylim(0.0,0.5)
+axs[2].set_ylim(0.0,0.5)
+axs[3].set_ylabel(r'$H_{S},C_{JS}$')
+axs[3].set_xlabel(r'$\epsilon$ $[days]$')
+axs[3].legend(loc='upper center', bbox_to_anchor=(0.5, 1.55),
+          ncol=3)
 fig.tight_layout()
 
 fig.savefig('methods_chaos.png',dpi=350)
--- ExternalForcings/Scripts/plot_bifurcation.py
+++ ExternalForcings/Scripts/plot_bifurcation.py
@@ -20,6 +20,7 @@ def hl_envelopes_idx(s, dmin=1, dmax=1, split=False):
 
     # locals min
     lmin = (np.diff(np.sign(np.diff(s))) > 0).nonzero()[0] + 1
+#   lmin = (np.diff(np.sign(np.diff(-s))) < 0).nonzero()[0] + 1
     # locals max
     lmax = (np.diff(np.sign(np.diff(s))) < 0).nonzero()[0] + 1
 
@@ -38,11 +39,15 @@ def hl_envelopes_idx(s, dmin=1, dmax=1, split=False):
 
     return lmin,lmax
 
+
+
+
 #ncname = 'PF_result_chaos.nc'
+ncname = 'result_chaos.nc'
 #ncname = 'noise_result_periodic.nc'
-ncname = 'noise_result_stationary.nc'
-#amplitudes = np.linspace(0,20,20)
-amplitudes = [10,30,90,180,365]
+#ncname = 'noise_result_stationary.nc'
+amplitudes = np.linspace(0,20,20)
+#amplitudes = [10,30,90,180,365]
 f_noise = nc.Dataset(ncname)
 varnames=['B1_c','P1_c','P2_c','P3_c','P4_c','Z5_c','Z6_c','Z3_c','Z4_c']
 t = np.linspace(0, 7300., 100000)
@@ -54,32 +59,76 @@ plt.subplots_adjust(bottom=0.12)
 #fig.tight_layout()
 axs = axs.ravel()
 titles = ['Bacteria B1', 'Diatoms P1', 'Nanoflagellates P2', 'Picophytoplankton P3', 'Dinoflagellates P4', 'Microzooplankton Z5', 'het. Nanoflagellates Z6', 'carn. Mesozooplankton Z3', 'omn. Mesozooplankton Z4']
+fig1,ax1 = plt.subplots(3,1,sharex=True)
+fig2,ax2 = plt.subplots()
+ax1 = ax1.ravel()
 for iax,ax in enumerate(axs):  
 #   if iax == 1:
 #       break   
     print('doing ',varnames[iax])
-    for i in range(len(f_noise.variables[varnames[iax]][:,0,0,0,0])):
+    lyap = f_noise.variables['max_lyapunov'][:,0,0,0]
+    for i in range(len(f_noise.variables[varnames[iax]][:15,0,0,0,0])):
+        if lyap[i] > 0.001:
+            c = 'rebeccapurple'
+        else:
+            c = 'dodgerblue'
         y = f_noise.variables[varnames[iax]][i,-50000:,0,0,0] #last three years timeseries
-        lmin, lmax = hl_envelopes_idx(y,dmin=410)
+        lmin, lmax = hl_envelopes_idx(y)#,dmin=410)
         for upper in y[lmax]:
-            ax.scatter(amplitudes[i],upper,s=1,marker='o',facecolors='none', edgecolors='k')
+            ax.scatter(amplitudes[i],upper,s=1,marker='o',facecolors='none', edgecolors=c)
         for lower in y[lmin]:
-            ax.scatter(amplitudes[i],lower,s=1,marker='o',facecolors='none', edgecolors='k')
+            ax.scatter(amplitudes[i],lower,s=1,marker='o',facecolors='none', edgecolors=c)
+        if varnames[iax] == 'P1_c':
+            if i == 1 :
+                ax1[0].plot(t[-50000:],y,c='k')
+                ax1[0].set_title(r'$A_y=1.0^\circ C$')
+                ax1[0].plot(t[-50000:][lmax],y[lmax],c='r')
+                ax1[0].plot(t[-50000:][lmin],y[lmin],c='r')
+            if i == 5 :
+                ax1[1].plot(t[-50000:],y,c='k')
+                ax1[1].set_title(r'$A_y=5.0^\circ C$')
+                ax1[1].plot(t[-50000:][lmax],y[lmax],c='r')
+                ax1[1].plot(t[-50000:][lmin],y[lmin],c='r')
+            if i == 10 :
+                ax1[2].plot(t[-50000:],y,c='k')
+                ax1[2].set_title(r'$A_y=10.0^\circ C$')
+                ax1[2].plot(t[-50000:][lmax],y[lmax],c='r')
+                ax1[2].plot(t[-50000:][lmin],y[lmin],c='r')
+            for upper in y[lmax]:
+                    ax2.scatter(amplitudes[i],upper,s=1,marker='o',facecolors='none', edgecolors=c)
+            for lower in y[lmin]:
+                    ax2.scatter(amplitudes[i],lower,s=1,marker='o',facecolors='none', edgecolors=c)
     ax.set_title(titles[iax],fontsize=8)
-    ax.set_xscale('log')
+    ax.set_xticks([5,10,15])
+#   ax.set_xscale('log')
 #   ax.tick_params(axis='both', which='major', labelsize=7)
+axs[8].set_xticklabels([5,10,15])
 
-
-fig.text(0.5, 0.04, r'$\tau$ [$days$]', ha='center')
-#fig.text(0.5, 0.04, 'Amplitude [$^\circ$C]', ha='center')
+#fig.text(0.5, 0.04, r'$\tau$ [$days$]', ha='center')
+fig.text(0.5, 0.04, 'Amplitude [$^\circ$C]', ha='center')
 fig.text(0.04, 0.5, 'Biomass [$mg C/m^3$]', va='center', rotation='vertical')
 #legend = [] 
 #legend.append(ax.get_legend_handles_labels())
 #legend.append(ax1.get_legend_handles_labels())
 
 #fig.tight_layout()
+fig1.text(0.04, 0.5, 'P1 Biomass [$mg C/m^3$]', va='center', rotation='vertical')
+ax1[2].set_xlabel('time [days]')
+fig1.tight_layout(rect=(0.1, 0.1, 0.9, 0.9))
+
+
+ax2.set_xlabel('Amplitude [$^\circ$C]')
+ax2.set_ylabel('Biomass [$mg C/m^3$]')
+
+
+
+
 
 fig.savefig('fluctuations.png', format='png',dpi=350)
+fig1.savefig('methods_envelope.png', format='png',dpi=350)
+fig2.savefig('methods_bifurcation.png', format='png',dpi=350)
 
+
+plt.show()
 
 
